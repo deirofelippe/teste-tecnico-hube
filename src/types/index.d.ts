f1af8c02b5288806c95fd8caa76881d8a2c7763b
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-23 16:43:13 -0300

    feat(types): cria arquivos com types e interfaces

---
+++ src/types/index.d.ts
@@ -0,0 +1,23 @@
+export type Result = {
+  errors: ValidatorErrors['errors'];
+  data: any;
+};
+
+export type ValidatorErrors = { errors: { message: string }[] };
+
+export interface IUsuarioRepository {
+  create(usuario: Usuario): void;
+  findOne(id: string): Usuario | undefined;
+}
+
+export interface Usuario {
+  nome: string;
+  email: string;
+  telefone: string;
+  dataNascimento: string;
+}
+
+export interface IDatabase {
+  getOne(id: string): Usuario | undefined;
+  put(data: Record<string, string>): void;
+}
