84732166dd04992188478f313034b1ffaa091a3b
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-23 16:17:18 -0300

    config(jest): adiciona configuracao do swc

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,20 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   clearMocks: true,
   moduleDirectories: ['node_modules', '<rootDir>/'],
   transform: {
-    '^.+.tsx?$': ['ts-jest', {}]
+    '^.+\\.(t|j)sx?$': '@swc/jest'
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'node',
-  collectCoverage: true,
+  collectCoverage: false,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   coverageReporters: ['json', 'text', 'lcov', 'clover'],
   collectCoverageFrom: ['./src/**'],
   coverageThreshold: {
     global: {
-      lines: 85
+      lines: 80
     }
   }
 };
