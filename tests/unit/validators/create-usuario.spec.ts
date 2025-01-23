b065a7a0e49ff51a67650e7c6251e3f6d67aa704
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-23 19:54:59 -0300

    tests(unit): cria testes para CreateUsuarioValidator

---
+++ tests/unit/validators/create-usuario.spec.ts
@@ -0,0 +1,76 @@
+import { Usuario } from '@src/types';
+import { CreateUsuarioValidator } from '@src/validators/create-usuario.ts';
+
+describe('CreateUsuarioValidator', () => {
+  describe('Erros', () => {
+    test.each([['      '], ['  a  '], ['aa']])(
+      'Deve dar erro no nome',
+      (nome) => {
+        const usuario: Usuario = {
+          nome: nome,
+          email: 'teste@teste.com.br',
+          telefone: '912345678',
+          dataNascimento: '18/11/1999'
+        };
+
+        const createUsuarioValidator = new CreateUsuarioValidator();
+        const validatorErrors = createUsuarioValidator.validate(usuario);
+
+        expect(validatorErrors.errors).toHaveLength(1);
+      }
+    );
+
+    test.each([['teste'], ['teste@e'], ['teste@E.']])(
+      'Deve dar erro no email',
+      (email) => {
+        const usuario: Usuario = {
+          nome: 'Teste',
+          email: email,
+          telefone: '11 9 2345 6789',
+          dataNascimento: '18/11/1999'
+        };
+
+        const createUsuarioValidator = new CreateUsuarioValidator();
+        const validatorErrors = createUsuarioValidator.validate(usuario);
+
+        expect(validatorErrors.errors).toHaveLength(1);
+      }
+    );
+
+    test.each([['teste'], ['teste@e'], ['teste@E.']])(
+      'Deve dar erro na dataDeNascimento',
+      (dataDeNascimento) => {
+        const usuario: Usuario = {
+          nome: 'Teste',
+          email: 'teste@teste.com.br',
+          telefone: '11 9 2345 6789',
+          dataNascimento: dataDeNascimento
+        };
+
+        const createUsuarioValidator = new CreateUsuarioValidator();
+        const validatorErrors = createUsuarioValidator.validate(usuario);
+
+        expect(validatorErrors.errors).toHaveLength(1);
+      }
+    );
+
+    test.each([['1234567'], ['123456789012']])(
+      'Deve dar erro no telefone',
+      (telefone) => {
+        const usuario: Usuario = {
+          nome: 'Teste',
+          email: 'teste@teste.com.br',
+          telefone: telefone,
+          dataNascimento: '18/11/1999'
+        };
+
+        const createUsuarioValidator = new CreateUsuarioValidator();
+        const validatorErrors = createUsuarioValidator.validate(usuario);
+
+        expect(validatorErrors.errors).toHaveLength(1);
+      }
+    );
+  });
+
+  describe('Sucessos', () => {});
+});
