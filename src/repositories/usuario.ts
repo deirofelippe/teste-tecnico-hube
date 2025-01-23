989a5919e1cc0addc7cc9c98e05afac925d252e6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-23 16:29:09 -0300

    feat(repositories): cria a classe UsuarioRepository

---
+++ src/repositories/usuario.ts
@@ -0,0 +1,17 @@
+import { Database } from '@src/database/database';
+import { IUsuarioRepository, Usuario } from '@src/types';
+
+export class UsuarioRepository implements IUsuarioRepository {
+  constructor(private database: Database) {}
+
+  public create(usuario: Usuario): void {
+    const id = Date.now().toString();
+    const novoUsuario = JSON.stringify(usuario);
+
+    this.database.put({ [id]: novoUsuario });
+  }
+
+  public findOne(id: string): Usuario | undefined {
+    return this.database.getOne(id);
+  }
+}
