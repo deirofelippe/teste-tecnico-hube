a605c8e63bb8f53eee520dcf50b638fa60d46fb6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-23 16:29:40 -0300

    feat(database): cria a classe Database

---
+++ src/database/database.ts
@@ -0,0 +1,46 @@
+import { IDatabase, Usuario } from '@src/types';
+
+export class Database implements IDatabase {
+  private static instance: Database;
+  private static usuarios: { [id: string]: string }[];
+
+  private constructor() {
+    Database.usuarios = [];
+  }
+
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+
+    return Database.instance;
+  }
+
+  public getOne(id: string): Usuario | undefined {
+    const usuarioBuscado = Database.usuarios.find(
+      (usuarioId): string | undefined => usuarioId[id]
+    );
+
+    if (!usuarioBuscado) {
+      return undefined;
+    }
+
+    const jsonUsuario = Object.values(usuarioBuscado)[0];
+
+    const usuario = JSON.parse(jsonUsuario) as Usuario;
+
+    return usuario;
+  }
+
+  public put(data: Record<string, string>): void {
+    Database.usuarios.push(data);
+  }
+
+  public clear(): void {
+    Database.usuarios = [];
+  }
+
+  public getAll() {
+    return Database.usuarios;
+  }
+}
