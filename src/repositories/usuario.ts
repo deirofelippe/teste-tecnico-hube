4c8dac5ee7e11bfca223e8ffe8c0a000dfff139b
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-23 22:41:12 -0300

    feat(repositories): adiciona id no retorno de UsuarioRepository@create

--- src/repositories/usuario.ts
+++ src/repositories/usuario.ts
@@ -4,11 +4,18 @@ import { IUsuarioRepository, Usuario } from '@src/types';
 export class UsuarioRepository implements IUsuarioRepository {
   constructor(private database: Database) {}
 
-  public create(usuario: Usuario): void {
+  public create(usuario: Usuario): UsuarioComID {
     const id = Date.now().toString();
     const novoUsuario = JSON.stringify(usuario);
 
     this.database.put({ [id]: novoUsuario });
+
+    const usuarioComId: UsuarioComID = {
+      ...usuario,
+      id: id
+    };
+
+    return usuarioComId;
   }
 
   public findOne(id: string): Usuario | undefined {
